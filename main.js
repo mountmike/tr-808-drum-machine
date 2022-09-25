8e175f20647d3a617db6928b6fc7503a12afbdd8
Michael <tharratt.michael@gmail.com>
2022-09-24 21:17:41 -0700

    Adding 808 Sample library

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-function makeTextBold()
-    {
-        var textStr = document.getElementById('turnCapsBold').innerHTML;
-        var outputStr = '';
-        for (var loop=0; loop<textStr.length; loop++)
-        {
-            if (textStr.charCodeAt(loop) >= 65 && textStr.charCodeAt(loop) <= 90)
-                outputStr += '<B>' + textStr.charAt(loop) + '</B>';
-            else
-                outputStr += textStr.charAt(loop);            
-        }
-        document.getElementById('turnCapsBold').innerHTML = outputStr;
-    }
-makeTextBold();
+// Loading all samples to variables
+const bassDrumSample = new Audio("TR808/BD/BD5050.wav");
+const snareDrumSample = new Audio("TR808/SD/SD5050.wav");
+const lowTomSample = new Audio("TR808/LT/LT10.wav");
+const midTomSample = new Audio("TR808/MT/MT10.wav");
+const hiTomSample = new Audio("TR808/HT/HT10.wav");
+const rimShotSample = new Audio("TR808/RS/RS.wav");
+const clapSample = new Audio("TR808/CP/CP.wav");
+const cowbellSample = new Audio("TR808/CB/CB.wav");
+const cymbalSample = new Audio("TR808/CY/CY0010.wav");
+const openHatSample = new Audio("TR808/OH/OH10.wav");
+const closedHatSample = new Audio("TR808/CH/CH.wav");
+
+
+// Defining sample buttons
+
+const bassDrumBtn = document.querySelector('#s1');
+
+bassDrumBtn.addEventListener('click', function() {
+    bassDrumSample.play();
+  });
